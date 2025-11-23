f2ff428594d42d41086780da1616acf0a38e3b63
Tareq1333 <developer1@bytechjo.com>
2025-11-23 09:53:53 +0300

    final update

--- src/pages/feedbackwrapper.jsx
+++ src/pages/feedbackwrapper.jsx
@@ -1,27 +1,38 @@
 import { useParams } from "react-router-dom";
 import { Suspense, lazy, useEffect, useState } from "react";
+import StoryLayout from "./StoryLayout.jsx";
+
+const pages = {
+  "One-1": lazy(() => import("../units/g1/unitOne/L1/feedBack.jsx")),
+  "One-2": lazy(() => import("../units/g1/unitOne/L2/feedBack.jsx")),
+  "One-3": lazy(() => import("../units/g1/unitOne/L3/feedBack.jsx")),
+
+  "Two-1": lazy(() => import("../units/g1/unitTwo/L1/feedBack.jsx")),
+  "Two-2": lazy(() => import("../units/g1/unitTwo/L2/feedBack.jsx")),
+  "Two-3": lazy(() => import("../units/g1/unitTwo/L3/feedBack.jsx")),
+
+  "Three-1": lazy(() => import("../units/g1/unitThree/L1/feedBack.jsx")),
+  "Three-2": lazy(() => import("../units/g1/unitThree/L2/feedBack.jsx")),
+  "Three-3": lazy(() => import("../units/g1/unitThree/L3/feedBack.jsx")),
+
+  "Four-1": lazy(() => import("../units/g1/unitFour/L1/feedBack.jsx")),
+  "Four-2": lazy(() => import("../units/g1/unitFour/L2/feedBack.jsx")),
+  "Four-3": lazy(() => import("../units/g1/unitFour/L3/feedBack.jsx")),
+};
+
 
 const FeedbackWrapper = () => {
   const { unitId, lessonId } = useParams();
-  const [FeedBackPageComponent, setFeedBackPageComponent] = useState(null);
+  const key = `${unitId}-${lessonId}`;
+  const Component = pages[key];
 
-  useEffect(() => {
-    const path = `../units/g1/unit${unitId}/L${lessonId}/FeedBack.jsx`;
-    
-    const FeedBackComponent = lazy(() => import(path));
-    setFeedBackPageComponent(FeedBackComponent);
-
-  }, [unitId, lessonId]); 
-
-  if (!FeedBackPageComponent) {
-    return <div>Loading...</div>;
-  }
+  if (!Component) return <div>feedback not found</div>;
 
   return (
-    < Suspense fallback={
-        <div>Loading...</div>
-    } >
-      <FeedBackPageComponent />
+    <Suspense fallback={<div>Loading...</div>}>
+      <StoryLayout>
+        <Component />
+      </StoryLayout>
     </Suspense>
   );
 };
--- src/pages/quizwrapper.jsx
+++ src/pages/quizwrapper.jsx
@@ -1,25 +1,38 @@
 import { useParams } from "react-router-dom";
 import { Suspense, lazy, useEffect, useState } from "react";
+import StoryLayout from "./StoryLayout.jsx";
+
+const pages = {
+  "One-1": lazy(() => import("../units/g1/unitOne/L1/QuizPage.jsx")),
+  "One-2": lazy(() => import("../units/g1/unitOne/L2/QuizPage.jsx")),
+  "One-3": lazy(() => import("../units/g1/unitOne/L3/QuizPage.jsx")),
+
+  "Two-1": lazy(() => import("../units/g1/unitTwo/L1/QuizPage.jsx")),
+  "Two-2": lazy(() => import("../units/g1/unitTwo/L2/QuizPage.jsx")),
+  "Two-3": lazy(() => import("../units/g1/unitTwo/L3/QuizPage.jsx")),
+
+  "Three-1": lazy(() => import("../units/g1/unitThree/L1/QuizPage.jsx")),
+  "Three-2": lazy(() => import("../units/g1/unitThree/L2/QuizPage.jsx")),
+  "Three-3": lazy(() => import("../units/g1/unitThree/L3/QuizPage.jsx")),
+
+  "Four-1": lazy(() => import("../units/g1/unitFour/L1/QuizPage.jsx")),
+  "Four-2": lazy(() => import("../units/g1/unitFour/L2/QuizPage.jsx")),
+  "Four-3": lazy(() => import("../units/g1/unitFour/L3/QuizPage.jsx")),
+};
+
 
 const QuizWrapper = () => {
   const { unitId, lessonId } = useParams();
-  const [QuizPageComponent, setQuizPageComponent] = useState(null);
+  const key = `${unitId}-${lessonId}`;
+  const Component = pages[key];
 
-  useEffect(() => {
-    const path = `../units/g1/unit${unitId}/L${lessonId}/QuizPage.jsx`;
-    
-    const QuizComponent = lazy(() => import(/* @vite-ignore */ path));
-    setQuizPageComponent(QuizComponent);
-
-  }, [unitId, lessonId]); 
-
-  if (!QuizPageComponent) {
-    return <div>Loading...</div>;
-  }
+  if (!Component) return <div>Quiz not found</div>;
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <QuizPageComponent />
+      <StoryLayout>
+        <Component />
+      </StoryLayout>
     </Suspense>
   );
 };
